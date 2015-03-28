bd3152741aff573397ee3f072293329bf80192c7
Potfur <wachowski.michal@gmail.com>
2015-03-28 18:22:29 +0100

    update for framework 1.2

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 
-$moss = new \Moss\Kernel\App(require __DIR__ . '/../src/Moss/Sample/bootstrap.php');
+$config = new \Moss\Config\Config(require __DIR__ . '/../src/Moss/Sample/bootstrap.php');
+
+$moss = new \Moss\Kernel\App($config);
 $moss->run()->send();
